b1c82b63b1b7f534a4d4640e01f8c3cfb5d4945a
Sylvain Pelissier <sylvain.pelissier@gmail.com>
2022-12-22 22:35:49 +0100

    Finish AES key schedule

--- aes.c
+++ aes.c
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 #include <byteswap.h>
 
-uint64_t aes_round_encrypt(const uint64_t state0, uint64_t state1);
-uint64_t aes_expand_key(const uint64_t *key, uint64_t *rk);
+void aes_encrypt(const uint64_t *rk, uint64_t *plaintext, uint64_t *ciphertext);
+void aes_expand_key(const uint64_t *key, uint64_t *rk);
 
 int main(int argc, char *argv[]) {
 	char key_str[17] = {'\0'};
 	char in_str[17] = {'\0'};
-    uint64_t rk[16] = {0};
+    uint64_t rk[22] = {0};
     uint64_t out[2] = {0};
     uint64_t in[2] = {0};
 	uint64_t key[2] = {0};
@@ -31,22 +31,22 @@ int main(int argc, char *argv[]) {
     
 	// Copy key
 	strncpy(key_str, argv[1], 16);
-	key[0] = strtoull(key_str, NULL, 16);
+	key[0] = bswap_64(strtoull(key_str, NULL, 16));
 	strncpy(key_str, argv[1] + 16, 16);
-	key[1] = strtoull(key_str, NULL, 16);
-	printf("%016lx%016lx\n", key[0], key[1]);
-	
-	out[0] = aes_expand_key(key, rk);
-	printf("%016lx\n", out[0]);
-	
+	key[1] = bswap_64(strtoull(key_str, NULL, 16));
+
+	aes_expand_key(key, rk);
+	printf("%016lx%016lx\n", bswap_64(rk[6]), bswap_64(rk[7]));
+	printf("%016lx%016lx\n", bswap_64(rk[20]), bswap_64(rk[21]));
+
 	// Copy plaintext
 	strncpy(in_str, argv[2], 16);
-	in[0] = strtoull(in_str, NULL, 16);
+	in[0] = bswap_64(strtoull(in_str, NULL, 16));
 	strncpy(in_str, argv[2] + 16, 16);
-	in[1] = strtoull(in_str, NULL, 16);
-	printf("%016lx%016lx\n", in[0], in[1]);
+	in[1] = bswap_64(strtoull(in_str, NULL, 16));
 
-	out[0] = aes_round_encrypt(in[0], in[1]);
-    printf("%016lx\n", out[0]);
+	printf("%016lx%016lx\n", bswap_64(in[0]), bswap_64(in[1]));
+	aes_encrypt(rk, in, out);
+    printf("%016lx%016lx\n", bswap_64(out[0]), bswap_64(out[1]));
     return 0;
 }
